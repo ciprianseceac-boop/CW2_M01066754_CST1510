6eaffe683aa27503971a9da4039f9b0b111d000f
ciprian140385 <CS1571@live.mdx.ac.uk>
2025-12-03 15:12:48 +0000

    update and create dashboard

--- app/data/db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from pathlib import Path
-
-DATA_DIR = Path("DATA")
-DATA_PATH = DATA_DIR / "intelligence_platform.db"
-
-conn = sqlite3.connect(DATA_PATH)  
-
--- app/users.py
+++ app/users.py
@@ -1,8 +1,9 @@
 import bcrypt
+from app.db import get_connection
 
 def hash_password(password: str) -> str:
     """Hash a password for storage."""        
-    salt = bcrypt.gensalt()
+    salt = bcrypt.gensalt() 
     hashed = bcrypt.hashpw(password.encode("utf-8"), salt)    
     return hashed.decode('utf-8')
 
@@ -10,7 +11,7 @@ def verify_password(provided_password: str, stored_password: str) -> bool:
     """Verify a stored password against one provided by user"""
     return bcrypt.checkpw(provided_password.encode('utf-8'), stored_password.encode('utf-8'))
 
-def set_user(conn, name, hash):
+def add_user(conn, name, hash):
     cur = conn.cursor()
     sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
     cur.execute(sql, (name, hash))
@@ -20,25 +21,20 @@ def get_one_user(conn, name):
     cur = conn.cursor()
     sql = "SELECT id, username, password_hash FROM users WHERE username = ?"
     cur.execute(sql, (name,))
-    return cur.fetchone()
+    user = cur.fetchone()
+    return user
 
-def login_user(conn):
-    name = input("Enter name to login: ")
-    password = input("Enter password to login: ")  
-    user = get_one_user(conn, name)
-    if not user:
-        print("User not found.")
-        return False
-    _, name_db, hash_db = user
-    if verify_password(password, hash_db):
-        print("Login successful.")
+def login_user(conn, name, password):
+    # name = input("Enter name to login: ")
+    # password = input("Enter password to login: ")   
+    id, name, hash = get_one_user(conn, name)
+    print(f'welcome {name}')
+    if name == name and verify_password(password, hash):
         return True
-    
-    print("Login failed.")
-    return False
-
-    print("Login failed.")
-    return False
+    else:
+        return False
+    #print("Login failed.")
+    #return False
 
 def get_all_users(conn):
     cur = conn.cursor()
@@ -64,11 +60,11 @@ def migrate_users(conn):
     with open('DATA\\users.txt', 'r') as f:
         for line in f:
             name, hash = line.strip().split(',')
-            set_user(conn, name, hash)
+            add_user(conn, name, hash)
 
 def register_username(conn):
     name = input("Enter new username: ").strip()
     password = input("Enter new password: ").strip()
     hashed = hash_password(password)
-    set_user(conn, name, hashed)
+    add_user(conn, name, hashed)
     print(f"User '{name}' registered successfully.")
